97da2af125b5220444eb7090305784d3682412bc
Lucas <AbbyNode@users.noreply.github.com>
2025-11-05 19:57:55 -0500

    init as borgmatic

--- setup/scripts/ofelia-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Create directory and symlink for ofelia config
-mkdir -p /etc/ofelia
-ln -sf /config/config.ini /etc/ofelia/config.ini
-
-# Execute ofelia
-exec /usr/bin/ofelia "$@"
